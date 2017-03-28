b24de5239ace342b1a3287621423706f03615705
KamalChauhan <kmlchauhan@gmail.com>
2017-03-28 17:55:43 +0530

    First Commit

---
+++ SeleniumParallelTest/Hooks.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Firefox;
+
+namespace SeleniumParallelTest
+{
+
+    [TestFixture]
+    public class Hooks : Base
+    {
+
+        public Hooks()
+        {
+            Driver = new FirefoxDriver();
+        }
+
+    }
+}
---
+++ SeleniumParallelTest/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace SeleniumParallelTest
+{
+
+    [TestFixture]
+    public class FirefoxTesting : Hooks
+    {
+
+        [Test]
+        public void FirefoxGoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("IRIS Software");
+            Driver.FindElement(By.Name("btnG")).Click();
+            Assert.That(Driver.PageSource.Contains("IRIS Software"), Is.EqualTo(true), "The text IRIS Software doent exist");
+        }
+    }
+
+    [TestFixture]
+    public class ChromeTesting : Hooks
+    {
+
+        [Test]
+        public void ChromeGoogleTest()
+        {
+            Driver.Navigate().GoToUrl("http://www.google.com");
+            Driver.FindElement(By.Name("q")).SendKeys("IRIS Software");
+            Driver.FindElement(By.Name("btnG")).Click();
+            Assert.That(Driver.PageSource.Contains("IRIS Software"), Is.EqualTo(true), "The text IRIS Software doent exist");
+        }
+    }
+}
